080ed9c06351baac3676862c5c787c946146836d
Ben <willcodefortea@gmail.com>
2023-02-18 17:19:22 +0000

    Add some comments for remap

--- nvim/lua/willcodefortea/remap.lua
+++ nvim/lua/willcodefortea/remap.lua
@@ -1,9 +1,11 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
+-- move lines up and down, keeping relevant indentation
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
+-- keep cursor in the middle fo the screen while jumping around
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
 vim.keymap.set("n", "n", "nzzzv")
