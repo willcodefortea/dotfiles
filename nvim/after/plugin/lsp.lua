97b7c23301875738cbfd12010c0d2f53061d8934
Ben <willcodefortea@gmail.com>
2023-02-18 17:12:56 +0000

    Update LSP config

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -3,11 +3,30 @@
 local lsp = require('lsp-zero')
 lsp.preset('recommended')
 
+local servers = {
+  pyright = {},
+  tsserver = {},
+  sumneko_lua = {
+    Lua = {
+      workspace = { checkThirdParty = false },
+       -- telemetry = { enable = false },
+      diagnostics = {
+          globals = { 'vim' }
+      }
+    },
+  },
+}
 
-lsp.ensure_installed({
-	'tsserver',
-	'eslint',
-	'sumneko_lua'
+lsp.ensure_installed(vim.tbl_keys(servers))
+
+lsp.configure('sumneko_lua', {
+    settings = {
+        Lua = {
+            diagnostics = {
+                globals = { 'vim' }
+            }
+        }
+    }
 })
 
 local cmp = require('cmp')
@@ -19,4 +38,38 @@ local cmp_mappings = lsp.defaults.cmp_mappings({
   ["<C-Space>"] = cmp.mapping.complete(),
 })
 
+lsp.setup_nvim_cmp({
+    mapping = cmp_mappings
+})
+
+lsp.set_preferences({
+    sign_icons = {
+        error = 'E',
+        warn = 'W',
+        hint = 'H',
+        info = 'I'
+    }
+})
+
+
+lsp.on_attach(function(client, bufnr)
+  local opts = {buffer = bufnr, remap = false}
+
+  vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
+  vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
+  vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
+  vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
+  vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
+  vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
+  vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
+  vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
+  vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
+  vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
+end)
+
+lsp.nvim_workspace()
 lsp.setup()
+
+vim.diagnostic.config({
+    virtual_text = true
+})
